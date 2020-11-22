aed561102a937212e869ef37e9d81be22b2d534f
Marlon Pacheco <pacheco.mjc@gmail.com>
2020-11-22 09:36:51 -0600

    server and router files setup. localhost now working

--- models/user.js
+++ models/user.js
@@ -3,6 +3,11 @@ const mongoose = require("mongoose"),
     bcrypt = require("bcryptjs"),
     SALT_WORK_FACTOR = 10;
 
+const validateEmail = function (email) {
+    var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return re.test(email)
+};
+
 const UserSchema = new Schema({
     email: {
         type: String,
@@ -33,34 +38,35 @@ const UserSchema = new Schema({
     },
     password: {
         type: String,
-        required: true }
+        required: true
+    }
 });
 
-UserSchema.pre(save, function(next) {
+UserSchema.pre(save, function (next) {
     const user = this;
 
-// only hash the password if it has been modified (or is new)
-if (!user.isModified('password')) return next();
+    // only hash the password if it has been modified (or is new)
+    if (!user.isModified('password')) return next();
 
-// generate a salt
-bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
-    if (err) return next(err);
-
-    // hash the password using our new salt
-    bcrypt.hash(user.password, salt, function(err, hash) {
+    // generate a salt
+    bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
         if (err) return next(err);
 
-        // override the cleartext password with the hashed one
-        user.password = hash;
-        next();
+        // hash the password using our new salt
+        bcrypt.hash(user.password, salt, function (err, hash) {
+            if (err) return next(err);
+
+            // override the cleartext password with the hashed one
+            user.password = hash;
+            next();
+        });
     });
-});
 
 
 });
 
-UserSchema.methods.comparePassword = function(candidatePassword, cb) {
-    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+UserSchema.methods.comparePassword = function (candidatePassword, cb) {
+    bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
         if (err) return cb(err);
         cb(null, isMatch);
     });
