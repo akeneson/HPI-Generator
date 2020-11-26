7650157455c910f08a44e4e6b91d21440fbafb16
Amy Keneson <amy.l.keneson@icloud.com>
2020-11-25 21:16:49 -0600

    Duration

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,18 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import logo from "./logo.svg";
 import "./App.css";
+import DurationPage from "./pages/Duration";
+import Navigation from "./components/Navigation";
 
 function App() {
   return (
     <Router>
       <div>
+        <Navigation />
         <h1>Hello HPI Generator</h1>
         <p>dang this was hard</p>
+        <DurationPage/>
+
       </div>
     </Router>
   );
---
+++ client/src/pages/Duration.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import Duration from "../components/Duration"
+
+const DurationPage = () => {
+  
+
+  return (
+    <React.Fragment>
+    <Duration/>
+ 
+
+    </React.Fragment>
+  );
+};
+
+export default DurationPage;
