a13364949dcfbd4a41fdfd3118c4c3c424ff8ec7
Marlon Pacheco <pacheco.mjc@gmail.com>
2020-12-01 21:56:03 -0600

    user signup validation completed

--- controllers/userController.js
+++ controllers/userController.js
@@ -15,11 +15,30 @@ module.exports = {
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
-  create: function (req, res) {
+  create: async function (req, res) {
+    const { email, password, firstName, lastName, gender, dob, passwordCheck } = req.body;
+    
+    if (!email || !password || !firstName || !lastName || !gender || !dob)
+      return res
+        .status(400)
+        .json({ msg: "Missing required fields" });
+    if (password.length < 4)
+      return res
+        .status(400)
+        .json({ msg: "Password must be at least 4 characters" });
+    if (password !== passwordCheck)
+      return res
+        .status(400)
+        .json({ msg: "Password check does not match" });
+    const existingUser = await db.User.findOne({email: email})
+    if (existingUser)
+      return res
+      .status(400)
+      .json({msg: "Email already in use" });
     db.User
       .create(req.body)
       .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
+      .catch(err => res.status(422).json({msg: "Must be a valid email address"}));
   },
   update: function (req, res) {
     db.User
