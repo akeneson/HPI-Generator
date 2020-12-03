4bfc44422c252ef3ff2267711c175da588936c4c
Robin Renneke <rennekerobin@yahoo.com>
2020-12-02 21:31:37 -0600

    updated registration page

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import logo from "./logo.svg";
 import "./App.css";
-import Navigation from "./components/Nav/Nav";
+import Register from "./components/Register/Register";
+import RegisterPage from "./components/Register/Register";
 
 function App() {
   return (
     <Router>
       <div>
-        <Navigation />
+        <RegisterPage />
       </div>
     </Router>
   );
--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -23,9 +23,9 @@ return(
           <Form.Control type="password" placeholder="Password" />
         </Form.Group>
   
-        <Form.Group controlId="formBasicPasswordTwo">
-          <Form.Label>PasswordTwo</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+        <Form.Group controlId="ConfirmPassword">
+          <Form.Label>Confirm Password</Form.Label>
+          <Form.Control type="password" placeholder="Confirm Password" />
           <Form.Text className="text-muted">
             Please re-enter your password.
           </Form.Text>
