b51b17a6d67dbea8682b9fe2204eee1c88d3c550
Amy Keneson <amy.l.keneson@icloud.com>
2020-12-03 12:43:01 -0700

    login page

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,19 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import logo from "./logo.svg";
 import "./App.css";
 import DurationPage from "./pages/Duration";
-import Home from "./pages/Home";
+import Home from "./components/Home/HomePage";
 import Navigation from "./components/Nav/Nav";
 // import Login from './components/Login';
 // import Register from './components/Register';
+import LoginPage from "./components/Login/LoginPage"
 
 function App() {
   return (
     <Router>
       <div>
-        <Navigation />
+        {/* <Navigation /> */}
+        <LoginPage />
+        
         {/* <Route exact path="/" component={Login}/>
         <Route path="/Register" component={Register}/> */}
       </div>
---
+++ client/src/components/Login/LoginPage.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Form, Button, Card } from "react-bootstrap";
+import './LoginPage.css';
+
+const LoginPage = () => {
+
+return(
+
+
+<Card style={{ width: '25rem'}}>
+    <Card.Body>
+
+<Form>
+  <Form.Group controlId="formBasicEmail">
+    <Form.Label>Email address</Form.Label>
+    <Form.Control type="email" placeholder="Enter email" />
+    <Form.Text className="text-muted">
+      We'll never share your email with anyone else.
+    </Form.Text>
+  </Form.Group>
+
+  <Form.Group controlId="formBasicPassword">
+    <Form.Label>Password</Form.Label>
+    <Form.Control type="password" placeholder="Password" />
+  </Form.Group>
+  
+  {/* this button should take the user to the patient feed page */}
+  <Button variant="primary" type="submit">
+    Submit
+  </Button>
+
+  {/* this button should take the user to the Provider Login Page */}
+  <Button variant="primary" type="submit" label="NewPatientButton">
+    New Patient
+  </Button>
+</Form>
+</Card.Body>
+</Card>
+
+);
+}
+export default LoginPage;
