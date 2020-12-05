7894e34f0043a35c6a25395213065418abd3efee
Amy Keneson <amy.l.keneson@icloud.com>
2020-12-04 21:03:28 -0600

    Nav bar

--- client/src/App.js
+++ client/src/App.js
@@ -2,21 +2,23 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Home from "./components/Home/HomePage";
-import Navigation from "./components/Nav/Nav";
+import Navigation from "./components/Navigation/Navigation";
 import HomePage from './components/Home/HomePage';
 import FeedPage from './components/Feed';
-import HpiCard from './components/HPI/HPICard';
-import history from './components/history';
+// import history from './components/history';
 // import Login from './components/Login';
 // import Register from './components/Register';
 import LoginPage from "./components/Login/LoginPage"
+import HPICard from "./components/HPI/HPICard";
 
 function App() {
   return (
     <Router>
       <div>
-        {/* <Navigation /> */}
-        <LoginPage />
+        <Navigation />
+        {/* <LoginPage /> */}
+        <HomePage />
+        <HPICard />
         
         {/* <Route exact path="/" component={Login}/>
         <Route path="/Register" component={Register}/> */}
--- client/src/components/Navigation/Navigation.js
+++ client/src/components/Navigation/Navigation.js
@@ -2,15 +2,16 @@ import React from "react";
 import { NavLink} from "react-router-dom";
 import { Navbar, Nav, Form} from "react-bootstrap";
 import Time from '../CurrTime/CurrTimePage'
+import "./Navigation"
 
 const Navigation = () => {
   return (
     <>
       <Navbar bg="primary" variant="light">
-        <Navbar.Brand href="#home">HPI</Navbar.Brand>
+        <Navbar.Brand href="#home" Text style={{color: "white"}}>Millennial Health Inc.</Navbar.Brand>
         <Nav className="mr-auto">
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/new">New Visit</NavLink>
+          <NavLink to="/" Text style={{color: "white"}}>Home </NavLink>
+          <NavLink to="/new" Text style={{color: "white"}} margin-left padding="10px"> New Visit</NavLink>
         </Nav>
         <Form inline className="time">
           <Time/>
@@ -21,3 +22,6 @@ const Navigation = () => {
 }
 
 export default Navigation;
+
+
+
