036e8e690112ec64ee9f50dc452d7425fbe697f8
Amy Keneson <amy.l.keneson@icloud.com>
2020-12-06 22:12:12 -0600

    attempt to style login page. Working, but not nicely styled.

--- client/src/App.js
+++ client/src/App.js
@@ -3,24 +3,25 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Home from "./components/Home/HomePage";
 import Navigation from "./components/Navigation/Navigation";
-import FeedPage from './components/Feed';
+// import FeedPage from './components/Feed';
 import HpiCard from './components/HPI/HPICard';
 // import history from './components/history';
 
 import Register from './components/Register/Register';
-import Login from "./components/Login/LoginPage"
+import Login from "./components/Login/LoginPage";
 
 function App() {
   return (
     <Router>
       <Navigation />
       <div>
+     
         <Switch>
           <Route exact path="/" component={Login} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/register" component={Register} />
           <Route exact path="/home" component={Home} />
-          <Route exact path="/hpi" component={HpiCard} />
+          <Route exact path="/hpi" component={HpiCard} /> 
           {/* <Route exact path="/user" component={Login}/> */}
         </Switch>
         {/* <Navigation /> */}
--- client/src/components/Navigation/Navigation.js
+++ client/src/components/Navigation/Navigation.js
@@ -1,23 +1,41 @@
 import React from "react";
 import { NavLink} from "react-router-dom";
-import { Navbar, Nav, Form} from "react-bootstrap";
+import { Navbar, Nav, Form, Jumbotron, Container} from "react-bootstrap";
 import Time from '../CurrTime/CurrTimePage'
-import "./Navigation"
+import "./Navigation";
+import bgimage from './BG.jpg'
+
+
+
 
 const Navigation = () => {
   return (
     <>
-      <Navbar bg="primary" variant="light">
-        <Navbar.Brand href="#home" Text style={{color: "white"}}>Millennial Health Inc.</Navbar.Brand>
-        <Nav className="mr-auto">
-          <NavLink to="/" Text style={{color: "white"}}>Home </NavLink>
-          <NavLink to="/new" Text style={{color: "white"}}> New Visit</NavLink>
-          <NavLink to="/new" Text style={{color: "white"}}> About us</NavLink>
-        </Nav>
-        <Form inline className="time">
-          <Time/>
-        </Form>
-      </Navbar>
+    
+    <Jumbotron style={{ backgroundImage: `url(${bgimage})` , backgroundSize: "cover"}}>
+    </Jumbotron>
+    <Navbar bg="primary" expand="lg">
+  <Navbar.Brand href="#home"></Navbar.Brand>
+  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <Navbar.Collapse id="basic-navbar-nav">
+    <Nav className="mr-auto">
+          <NavLink to="/" Text style={{color: "white", padding: 30, fontSize: 25}}>Home </NavLink>
+          <NavLink to="/new" Text style={{color: "white", padding: 30, fontSize: 25}}> New Visit</NavLink>
+          <NavLink to="/new" Text style={{color: "white", padding: 30, fontSize: 25}}> About us</NavLink>
+      
+    </Nav>
+        <Navbar.Brand href="#home" style={{alignSelf:"center"}} Text style={{color: "white" , fontSize: 50 , fontFamily: "Sacromento" }}>Millennial Health Inc.</Navbar.Brand>
+    <Time/>
+  </Navbar.Collapse>
+</Navbar>
+
+
+      {/* <Navbar bg="primary" variant="light">
+        <Navbar.Brand href="#home" Text style={{color: "white" , fontSize: 50 , fontFamily: "Sacromento" }}>Millennial Health Inc.</Navbar.Brand>
+       
+          
+     
+      </Navbar> */}
     </>
   )
 }
