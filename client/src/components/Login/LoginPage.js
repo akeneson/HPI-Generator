ef7826e30794b71a242aba11fd492b98cf27ab5f
Amy Keneson <amy.l.keneson@icloud.com>
2020-12-10 22:42:32 -0600

    debugging with marlon

--- client/src/components/Login/LoginPage.js
+++ client/src/components/Login/LoginPage.js
@@ -1,9 +1,10 @@
+
 import React, { useState, useContext }  from 'react';
 import { useHistory } from 'react-router-dom';
 import UserContext from '../../Context/UserContext';
 import Axios from 'axios';
 import ErrorNotice from "../Error/ErrorNotice"
-import { Form, Button, Card } from "react-bootstrap";
+import { Form, Button, Card, Row } from "react-bootstrap";
 import './LoginPage.css';
 
 const LoginPage = () => {
@@ -35,29 +36,65 @@ const LoginPage = () => {
 
 return(
 
-  <div className="page">
-  <h2>Log in</h2>
+
+  <Row className="d-flex justify-content-center px-5">
+<Card className="col-md-4 bg-white">
+  <Card.Header className="bg-pink">
+    <Card.Title>Log In</Card.Title>
+  
+  </Card.Header>
+  <Card.Body>
+    <Card.Text><Form className="form">
+  <Form.Group controlId="formBasicEmail">
   {error && (
     <ErrorNotice message={error} clearError={() => setError(undefined)} />
   )}
-  <form className="form" onSubmit={submit}>
-    <label htmlFor="login-email">Email</label>
-    <input
-      id="login-email"
-      type="email"
-      onChange={(e) => setEmail(e.target.value)}
-    />
+    <Form.Label>Email address</Form.Label>
+    <Form.Control type="email" placeholder="Enter email" />
+    <Form.Text className="text-muted">
+      We'll never share your email with anyone else.
+    </Form.Text>
+  </Form.Group>
 
-    <label htmlFor="login-password">Password</label>
-    <input
-      id="login-password"
-      type="password"
-      onChange={(e) => setPassword(e.target.value)}
-    />
+  <Form.Group controlId="formBasicPassword">
+    <Form.Label>Password</Form.Label>
+    <Form.Control type="password" placeholder="Password" />
+  </Form.Group>
+  <Form.Group controlId="formBasicCheckbox">
+    <Form.Check type="checkbox" label="Check me out" />
+  </Form.Group>
+  <Button variant="primary" type="submit">
+    Submit
+  </Button>
+</Form>
+    </Card.Text>
+    <Button className="btn-pink">Go somewhere</Button>
+  </Card.Body>
+</Card>
+</Row>
 
-    <input type="submit" value="Log in" />
-  </form>
-</div>
+//   <h1>Log in</h1>
+//   {error && (
+//     <ErrorNotice message={error} clearError={() => setError(undefined)} />
+//   )}
+//   <form className="form" onSubmit={submit}>
+//     <label htmlFor="login-email">Email</label>
+//     <input
+//       id="login-email"
+//       type="email"
+//       onChange={(e) => setEmail(e.target.value)}
+//     />
+
+//     <label htmlFor="login-password">Password</label>
+//     <input
+//       id="login-password"
+//       type="password"
+//       onChange={(e) => setPassword(e.target.value)}
+//     />
+
+//     <input type="submit" value="Log in" />
+//   </form>
+// </div>
 
 // <Card style={{ width: '25rem'}}>
 //     <Card.Body>
