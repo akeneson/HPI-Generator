78d57e45d02f22cc1b515721c1d064b73265e56b
Marlon Pacheco <pacheco.mjc@gmail.com>
2020-12-12 15:22:15 -0600

    redirect feed if not logged in

--- client/src/components/Feed.js
+++ client/src/components/Feed.js
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import HPIFeedCell from "./HPIFeedCell";
+import { useHistory } from 'react-router-dom';
+import UserContext from '../Context/UserContext';
+
 
 const FeedPage = () => {
+  const { userData } = useContext(UserContext);
+  const history = useHistory();
+  const user = [userData.user]
+
+
+   //redirects user to login page if not logged in
+   useEffect(() => {
+    if (!userData.user) history.push("/login")
+    console.log("HPI Card userData: ", [userData])
+     }) 
+
+
     return (
         <Container className="my-4">
             <Row>
@@ -13,7 +28,6 @@ const FeedPage = () => {
             </Row>
         </Container>
       );
-
 }
 
 export default FeedPage;
